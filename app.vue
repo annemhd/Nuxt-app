b0bef9bf2da407b2cf20036bf3f6005566bc600e
Anne-Catherine MICHAUD <71764180+annemhd@users.noreply.github.com>
2023-04-22 01:43:10 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script setup>
+import axios from 'axios'
+
+async function getUser() {
+  try {
+    const response = await axios.get('http://localhost:8080/API/user');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+</script>

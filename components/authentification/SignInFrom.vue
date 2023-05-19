b759b5b663a3b06a4c73cd638a23bd6d847d136d
Anne-Catherine MICHAUD <71764180+annemhd@users.noreply.github.com>
2023-05-19 11:22:56 +0200

    Base

--- components/authentification/SignInFrom.vue
+++ components/authentification/SignInFrom.vue
@@ -1,13 +1,30 @@
 <template>
     <el-form class="flex flex-col mb-4">
-        <el-input v-model="email" type="text" name="email" placeholder="Email" class="mb-2" :validate-event="false"/>
-        <el-input v-model="password" type="password" name="password" placeholder="Mot de passe" class="mb-4" show-password/>
+        <el-input
+            v-model="email"
+            type="text"
+            name="email"
+            placeholder="Email"
+            class="mb-2"
+            :validate-event="false"
+        />
+        <el-input
+            v-model="password"
+            type="password"
+            name="password"
+            placeholder="Mot de passe"
+            class="mb-4"
+            show-password
+        />
         <div class="flex justify-center">
-            <el-button type="primary" size="large" @click="submitForm" :disabled="disabled">Se connecter</el-button>
+            <el-button type="primary" size="large" @click="submitForm" :disabled="disabled"
+                >Se connecter</el-button
+            >
         </div>
     </el-form>
 </template>
 <script setup>
+import Cookies from 'js-cookie'
 import jwt from 'jwt-encode'
 import Module from '/services/user.js'
 import crypto from 'crypto-js'
@@ -16,16 +33,19 @@ const router = useRouter()
 const email = ref(null)
 const password = ref(null)
 const errors = ref([])
-const disabled = computed(() => { return email.value && password.value ? false : true })
+const disabled = computed(() => {
+    return email.value && password.value ? false : true
+})
 const emit = defineEmits(['goToDashboard'])
 const validEmail = (email) => {
-     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(email);
+    const regex =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return regex.test(email)
 }
 const submitForm = async (e) => {
     errors.value = []
     !email.value ? errors.value.push('Saisis ton email') : null
-    !validEmail(email.value) ? errors.value.push('L\'email est invalide') : null
+    !validEmail(email.value) ? errors.value.push("L'email est invalide") : null
     !password.value ? errors.value.push('Saisis un mot de passe') : null
     !errors.value.length ? true : false
     e.preventDefault()
@@ -33,14 +53,13 @@ const submitForm = async (e) => {
         try {
             const hashPwd = crypto.MD5(password.value).toString()
             const user = await Module.authentification(email.value, hashPwd)
-            const u = useCookie("user")
-            const payload = user 
-            const secret = 'xxx';
-            const token = jwt(payload, secret);
-            u.value = token
+            const payload = user
+            const secret = 'xxx'
+            const token = jwt(payload, secret)
+            Cookies.set('user', token)
             // window.location.replace("/dashboard")
-            router.push({ path: "/dashboard" });
-        } catch(e) {
+            router.push({ path: '/dashboard' })
+        } catch (e) {
             console.log('nope')
         }
     }
